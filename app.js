890d04af4893fa6e70c0ee96307dfbe06d3f5388
Piejko Grzegorz <grzegorz.piejko@bwigroup.com>
2020-07-25 16:28:16 +0200

    modyfikacja plików app.js, index.html, style.css

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
-let name="Grzegorz ";
-let lastname= "Piejko";
+let arrowRight = document.querySelector("#arrow-right");
 
-name2="Wojtek";
-lastname2="Pawłowski";
+let function3 = () => {
+  console.log("strzałka kliknięta!");
+};
 
-let person1 = {
-
-    name: "Grzegorz",
-    lastname: "Piejko"
-}
-
-let person2 = {
-
-    name: "Wojtek",
-    lastname: "Pawłowski"
-}
-
-
-console.log(person1.name +"" + person1.lastname);
-console.log(person2.name +"" + person2.lastname);
+arrowRight.addEventListener("click", function3);
