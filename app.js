e5cebedc00a9380231864e677f177a6cf9b332f2
Piejko Grzegorz <grzegorz.piejko@bwigroup.com>
2020-07-25 15:06:03 +0200

    dodanie elementów slidera

---
+++ app.js
@@ -0,0 +1,21 @@
+let name="Grzegorz ";
+let lastname= "Piejko";
+
+name2="Wojtek";
+lastname2="Pawłowski";
+
+let person1 = {
+
+    name: "Grzegorz",
+    lastname: "Piejko"
+}
+
+let person2 = {
+
+    name: "Wojtek",
+    lastname: "Pawłowski"
+}
+
+
+console.log(person1.name +"" + person1.lastname);
+console.log(person2.name +"" + person2.lastname);
