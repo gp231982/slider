4ffee56ecf869b189b73a6e333103f3c94ed130f
Piejko Grzegorz <grzegorz.piejko@bwigroup.com>
2020-07-26 12:57:17 +0200

    Nowe zmiany

--- app.js
+++ app.js
@@ -5,3 +5,97 @@ let function3 = () => {
 };
 
 arrowRight.addEventListener("click", function3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let dlugoscNapisu = (napis) => {
+//   return napis.length;
+// };
+
+// let doKwadratu = (x) => {
+//   let wynik = x * x;
+//   return wynik;
+// };
+
+// let dlugosc = dlugoscNapisu("ABCDEFG");
+// let kwadrat = doKwadratu(dlugosc);
+
+// console.log(kwadrat);
+
+// // let funkcjaLiniowa = (a, x, b) => {
+// //   let wynik = a * x + b;
+// //   return wynik;
+// // };
+
+// // funkcjaLiniowa(10, 2, 20);
+// // console.log(funkcjaLiniowa(10, 2, 20));
+
+// // let PLNtoEUR = (zloty) => {
+// //   let wynik = 0.23 * zloty;
+// //   return wynik;
+// // };
+
+// // console.log(PLNtoEUR(10));
+
+// // let EURtoUSD = (euro) => {
+// //   let wynik = 1.17 * euro;
+// //   return wynik;
+// // };
+
+// // console.log(EURtoUSD(10));
+
+// // let convert = (kwota, kursWymiany) =>{
+// //  let wynik = kwota*kursWymiany;
+// //  return wynik;
+// // }
+
+// // let PLNtoEUR = (zloty) => {
+// //   let wynik = convert(zloty,0.23)
+// //   return wynik;
+// // };
+
+
+// // let EURtoUSD = (euro) => {
+// //   let wynik = convert(euro,1.17);
+// //   return wynik;
+// // };
+
+// // let euro = PLNtoEUR(100);
+// // let usd = EURtoUSD(euro);
+
+// // console.log(euro);
+// // console.log(usd);
+
+
+// let Potega = (liczba, wykladnik) =>{
+// let wynik = Math.pow(liczba, wykladnik);
+// return wynik;
+// }
+// console.log(Potega(2,10));
+// console.log(Potega(4,10));
+// console.log(Potega(3,10));
+// console.log(Potega(3,10));
+// console.log(Potega(3,10));
+// console.log(Potega(1,10));
+// console.log(Potega(20,10));
