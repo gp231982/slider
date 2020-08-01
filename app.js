4722cb2a0d5142bded3ebeba290ca1f9d33d1c1b
Piejko Grzegorz <grzegorz.piejko@bwigroup.com>
2020-08-01 11:53:04 +0200

    działanie slidera za pomocą strzałek left i right oraz strzałek na klawiaturze

--- app.js
+++ app.js
@@ -2,89 +2,103 @@
 // 1. Znajdźmy kropkę nr. 2 - done
 // 2. Niech kropka 'slucha' na kliknięcie - done
 // 3. Po kliknięciu, niech pojawi się obrazek nr. 2 - done
-  //3.1 Znaleźć obrazek 2 - done
-  //3.2 Dodaćdo obrazka 2 klasę "active"-done
-  //3.3 Usunć klasę active z obrazka 1
-    //3.3.1 - Znaleźć obrazek 1 - done
+//3.1 Znaleźć obrazek 2 - done
+//3.2 Dodaćdo obrazka 2 klasę "active"-done
+//3.3 Usunć klasę active z obrazka 1
+//3.3.1 - Znaleźć obrazek 1 - done
+
+let activeSlideNumber = 1;
 
 let dot1 = document.querySelector("#dot1");
 let dot2 = document.querySelector("#dot2");
 let dot3 = document.querySelector("#dot3");
 let dot4 = document.querySelector("#dot4");
 
-let hideActiveElement = () =>{
+let numberOFSlides = document.querySelectorAll(".slide").length;
+
+let hideActiveElement = () => {
   let activeElement = document.querySelector(".active");
-  activeElement.classList.remove("active");
+  if (activeElement) {
+    activeElement.classList.remove("active");
+  }
 
   let activeDot = document.querySelector(".dot-active");
-activeDot.classList.remove("dot-active");
-
+  activeDot.classList.remove("dot-active");
 };
 
-
-
 let showSlide = (slideNumber) => {
+  activeSlideNumber = slideNumber;
   hideActiveElement();
   document.querySelector("#slide" + slideNumber).classList.add("active");
   document.querySelector("#dot" + slideNumber).classList.add("dot-active");
-}
+};
 
 let showSlide1 = () => {
   showSlide(1);
 };
 
-
 let showSlide2 = () => {
   showSlide(2);
 };
 
-let showSlide3 = () =>{
+let showSlide3 = () => {
   showSlide(3);
-}
+};
 
 let showSlide4 = () => {
   showSlide(4);
 };
 
+let showNextSlide = function () {
+  //to jest to samo co zwykła funkcja strzałkowa jesli w środku funkcji nie ma "this".
+  if (activeSlideNumber === numberOFSlides) {
+    showSlide(1);
+  } else {
+    showSlide(activeSlideNumber + 1);
+  }
+};
+
+let showPreviousSlide = () => {
+  //to jest to samo co zwykła funkcja strzałkowa jesli w środku funkcji nie ma "this".
+  if (activeSlideNumber === 1) {
+    showSlide(numberOFSlides);
+  } else {
+    showSlide(activeSlideNumber - 1);
+  }
+};
 
 dot1.addEventListener("click", showSlide1); //jeśli ktoś cię strzałko kliknie to zrób to co mówi funkcja showSlide2
 dot2.addEventListener("click", showSlide2);
 dot3.addEventListener("click", showSlide3);
 dot4.addEventListener("click", showSlide4);
 
+//1. znajdz strzałkę w prawo//
+//2. POkaż nastepny slide po kliknięciu na strząłkę w prawo //
 
+let arrowRight = document.querySelector("#arrow-right"); // szkanie elementu o danym Id//
+let arrowLeft = document.querySelector("#arrow-left");
 
+arrowRight.addEventListener("click", showNextSlide);
+arrowLeft.addEventListener("click", showPreviousSlide);
 
+let onKeyDown = (event) => {
+  if (event.keyCode === 39) {
+    showNextSlide();
+  } 
+  
+  if (event.keyCode === 37) {
+    showPreviousSlide();
+  } 
+};
 
+//jeśli klikamy strzałkę w prawo
+//showNextSlide();
+//Jeśli klikamy strzałkę w lewo
+//showPreviousSlide();
 
+document.addEventListener("keydown", onKeyDown);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+showSlide(activeSlideNumber);
 
 // let dlugoscNapisu = (napis) => {
 //   return napis.length;
